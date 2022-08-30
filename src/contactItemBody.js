0ff4e15bfdbd2a5097973d72d07b4843a0fbcc46
tyoprataama <tyoprataama004@gmail.com>
2022-08-30 13:55:41 +0700

    contact app

---
+++ src/contactItemBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function contactItemBody (name, tag) {
+    return (
+        <div className='contact-item_body'>
+            <h3 className='contact-item_title'>{name}</h3>
+            <p className='contact-item_username'>@{tag}</p>
+        </div>
+    );
+}
+
+export default contactItemBody;
