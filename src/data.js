e01d6bc28b2ac7497ca8ed6475156844a4af3eba
tyoprataama <tyoprataama004@gmail.com>
2022-08-30 14:57:15 +0700

    chsnge image directory

--- src/data.js
+++ src/data.js
@@ -4,19 +4,19 @@ const getData = () => {
             id: 1,
             name: 'Dimas Saputra',
             tag: 'dimasmds',
-            imageUrl: '../images/dimasmds.jpeg',
+            imageUrl: '/images/dimasmds.jpeg',
         },
         {
             id: 2,
             name: 'Arif Faizin',
             tag: 'arifaizin',
-            imageUrl: '../images/arifaizin.jpeg',
+            imageUrl: '/images/arifaizin.jpeg',
         }, {
             id: 3,
             name: 'Rahmat Fajri',
             tag: 'rfajri27',
-            imageUrl: '../images/rfajri27.jpeg',
+            imageUrl: '/images/rfajri27.jpeg',
         }, 
     ];
 }
-export {getData};
+export { getData };
