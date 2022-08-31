bc2b16c9d128bf5764a67ecf28db20d22c032c67
tyoprataama <tyoprataama004@gmail.com>
2022-08-31 21:07:30 +0700

    change directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import ContactApp from "./contactApp";
+import ContactApp from "./components/contactApp";
+import './styles/style.css'
 
 
 const root = createRoot(document.getElementById('root'));
